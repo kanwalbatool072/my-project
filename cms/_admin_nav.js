6a57a3451b90d800271e66cdadf4d7d0b714cb52
Israrminhas1 <47710475+Israrminhas1@users.noreply.github.com>
2021-04-18 16:45:47 +0500

    add userr

--- cms/_admin_nav.js
+++ cms/_admin_nav.js
@@ -1,4 +1,5 @@
 export default {
+<<<<<<< Updated upstream
   items: [
   //   // {
   //   // name: 'Dashboard',
@@ -19,6 +20,55 @@ export default {
         url: '/user',
         icon: 'icon-users'
       },
+=======
+    items: [
+        //   // {
+        //   // name: 'Dashboard',
+        //   // url: '/admin',
+        //   // icon: 'icon-speedometer',
+        //   // badge: {
+        //   //   variant: 'primary',
+        //   //   text: 'NEW'
+        //   // }
+        // },
+        {
+            name: 'Users',
+            url: '',
+            icon: 'icon-users',
+            children: [{
+                    name: 'Active users',
+                    url: '/user',
+                    icon: 'icon-users'
+                },
+                {
+                    name: 'Add user',
+                    url: '/adduser',
+                    icon: 'icon-users'
+                },
+            ]
+        },
+        {
+            name: 'Groups',
+            url: '',
+            icon: 'icon-speedometer',
+            children: [{
+                    name: 'Active group',
+                    url: '/user',
+                    icon: 'icon-user',
+                },
+                {
+                    name: 'Manage group',
+                    url: '',
+                    icon: 'icon-user',
+                }
+
+            ],
+            badge: {
+                variant: 'primary',
+                text: 'NEW'
+            }
+        }
+>>>>>>> Stashed changes
     ]
   },
   {
